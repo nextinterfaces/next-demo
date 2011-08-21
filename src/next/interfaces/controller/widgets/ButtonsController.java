7bc0737ad7ba6be622ca2f820590ca9c1a378479
next Interfaces <contact@nextinterfaces.com>
2011-08-21 00:50:24 -0700

    using CSS3 background gradients for buttons instead of image

--- src/next/interfaces/controller/widgets/ButtonsController.java
+++ src/next/interfaces/controller/widgets/ButtonsController.java
@@ -22,36 +22,55 @@ import next.i.view.widgets.XButton;
 import next.i.view.widgets.XButton.XButtonType;
 import next.interfaces.controller.GitXController;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 public class ButtonsController extends GitXController {
 
 	public ButtonsController() {
 		setTitle("Buttons");
+
+		RootLayoutPanel.get().getElement().addClassName("rootLayoutPanel222");
 	}
 
 	@Override
 	public IsWidget getViewContent() {
 
-		CellData d0 = new CellData(null, "Rounded Default", new XButton("Done"));
-		XTableCell c0 = new XTableCell(d0);
-
-		CellData d1 = new CellData(null, "Navigation", new XButton("Done", XButtonType.Navigation));
-		XTableCell c1 = new XTableCell(d1);
-
-		CellData d2 = new CellData(null, "Shadow", new XButton("Done", XButtonType.Shadow));
-		XTableCell c2 = new XTableCell(d2);
-
-		CellData d3 = new CellData(null, "Add", new XButton("Item", XButtonType.Add));
-		XTableCell c3 = new XTableCell(d3);
-
-		CellData d4 = new CellData(null, "Disclosure", new XButton("Preview", XButtonType.Disclosure, false));
-		XTableCell c4 = new XTableCell(d4);
+		CellData d0 = new CellData(null, "Rounded", new XButton("Rounded"));
+		CellData d1 = new CellData(null, "Shadow", new XButton("Shadow", XButtonType.Shadow));
+		CellData d2 = new CellData(null, "Navigation", new XButton("Navigation", XButtonType.Navigation));
+		CellData d3 = new CellData(null, "Blue", new XButton("Blue", XButtonType.NavigationBlue));
+		CellData d4 = new CellData(null, "Red", new XButton("Red", XButtonType.NavigationRed));
+		CellData d5 = new CellData(null, "Black", new XButton("Black", XButtonType.NavigationBlack));
+		CellData d6 = new CellData(null, "Image", new XButton("Left", XButtonType.Image));
+		CellData d7 = new CellData(null, "Image", new XButton("Right", XButtonType.Image, false));
 
 		XTableView tableView = new XTableView();
-		tableView.addItem(c0, c1, c2, c3, c4);
+		tableView.addItem(new XTableCell(d0), new XTableCell(d1), new XTableCell(d2), new XTableCell(d3),
+				new XTableCell(d4), new XTableCell(d5), new XTableCell(d6), new XTableCell(d7));
+
+		addClickHandlers(d0, d1, d2, d3, d4, d5, d6, d7);
 
 		return tableView;
+	}
+
+	private void addClickHandlers(CellData... data) {
+		for (CellData c : data) {
+			final XButton b = (XButton) c.getEastWidgets()[0];
+			b.addClickHandler(new ClickHandler() {
+				public void onClick(ClickEvent event) {
+					Window.alert("You tapped " + b.getTitle() + ".");
+					// XPopup popup = new XPopup();
+					// popup.setWidget(new XLabel("You tapped " + b.getTitle() + "."));
+					// popup.setTop(30.0, Unit.PCT); popup.setRight(30.,Unit.PCT);
+					// popup.setBottom(30., Unit.PCT); popup.setLeft(30., Unit.PCT);
+					// popup.show();
+				}
+			});
+		}
 	}
 
 	public String getGitPath() {
